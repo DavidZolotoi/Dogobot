eac3c6d722b2c102a4711e820a97f0721b916719
David <zolotoi77@inbox.ru>
2024-03-30 20:05:33 +0300

    Загрузил проект с start.spring.io.

---
+++ src/main/java/ru/dogobot/Dogobot/DogobotApplication.java
@@ -0,0 +1,13 @@
+package ru.dogobot.Dogobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogobotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/dogobot/Dogobot/DogobotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dogobot.Dogobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogobotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
